2d3053101dcacc2fdce930c6819d7d08f827fb35
Aichu Tan <aycu2004@gmail.com>
2026-09-11 10:52:35 -0700

    test

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ Musk is a supporter of global far-right politics, figures, and political parties
         input_variables=["information"], template = summary_template
     )
 
-    # llm = ChatOpenAI(temperature = 0, model= "gpt-5")
-    llm = ChatOllama(temperature = 0, model= "gemma3:270m")
+    llm = ChatOpenAI(temperature = 0, model= "gpt-5")
+    # llm = ChatOllama(temperature = 0, model= "gemma3:270m")
     chain = summary_prompt_template | llm
     
     response = chain.invoke({"information": information}) 
